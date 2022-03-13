505497d6947c8bc0f931bcb8df828299bf8576a4
Marlon Aguirre <marlon.aguirre@fuelcontrol.com.co>
2022-03-13 11:00:49 -0500

    Se agrega filtro de sesion y controlador de perfiles

--- RedSocial/App_Start/FilterConfig.cs
+++ RedSocial/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace RedSocial
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new Filtros.VerificarSesion());
         }
     }
 }
---
+++ RedSocial/Filtros/VerificarSesion.cs
@@ -0,0 +1,20 @@
+﻿using RedSocial.Controllers;
+using System.Web.Mvc;
+
+namespace RedSocial.Filtros
+{
+    public class VerificarSesion : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (!System.Web.HttpContext.Current.User.Identity.IsAuthenticated)
+            {
+                if (!(filterContext.Controller is AccountController))
+                {
+                    filterContext.HttpContext.Response.Redirect("~/Account/Login");
+                }
+            };
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
