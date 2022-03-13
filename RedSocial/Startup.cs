8a13ed9cd9c8f504ebcf33cfcbd2552f17122b51
Marlon Aguirre <marlon.aguirre@fuelcontrol.com.co>
2022-03-12 20:59:41 -0500

    Add project files.

---
+++ RedSocial/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RedSocial.Startup))]
+namespace RedSocial
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
